a1b2cbfe4d4928ed32f34a4e6d237e5a45b5025b
Daniel Jardim <daniel.azevedojardim@gmail.com>
2022-10-07 00:48:25 -0300

    Mais paginas adicionadas

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(function(){
+    var timeOut = setTimeout(function(){
+        $(".spinner-border").removeClass("spinner-border");
+        $(".main-img").removeClass("visually-hidden");
+    },1500);
+})
